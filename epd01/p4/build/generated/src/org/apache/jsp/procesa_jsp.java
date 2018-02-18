2e4cf70a3efb25a4cdd6dd12e870c354bdc15679
SusanaMci <susana.delacalle.iglesias@hotmail.com>
2018-02-18 17:29:26 +0100

    Add files via upload

---
+++ epd01/p4/build/generated/src/org/apache/jsp/procesa_jsp.java
@@ -0,0 +1,175 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class procesa_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  static {
+    _jspx_dependants = new java.util.ArrayList<String>(1);
+    _jspx_dependants.add("/footer.jsp");
+  }
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("         <meta charset=\"UTF-8\">\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"estilo.css\">\n");
+      out.write("        <title>EPD1-p1</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("    \n");
+      out.write("           <!--        Vista de formulario-->\n");
+      out.write("        <p>El siguiente formulario se utilizar&aacute; para saber si usted es apto para recibir un cr&eacute;dito</p>\n");
+      out.write("        <p>Rellene el siguiente el formulario:</p>\n");
+      out.write("        <form action=\"procesa.jsp\" method=\"post\">\n");
+      out.write("\n");
+      out.write("            <p> <label>Nombre: </label><input type=\"text\" name=\"nombre\"> </p>\n");
+      out.write("\n");
+      out.write("            <p><label>Apellidos: </label><input type=\"text\" name=\"apellidos\"> </p>\n");
+      out.write("\n");
+      out.write("            <p><label>Estado civil: </label><select name=\"estadoCivil\">\n");
+      out.write("\n");
+      out.write("                    <option value=\"soltero\">Soltero</option>\n");
+      out.write("                    <option value=\"casado\">Casado</option>\n");
+      out.write("                    <option value=\"casado\">Otros</option>\n");
+      out.write("                </select></p>\n");
+      out.write("\n");
+      out.write("            <p><label>¿Trabaja?: </label><label>S&iacute;</label><input type=\"radio\" name=\"trabaja\" value=\"si\">\n");
+      out.write("                <label>No</label><input type=\"radio\" name=\"trabaja\" value=\"no\"></p>\n");
+      out.write("\n");
+      out.write("            <p><label>Ingresos: </label><input type=\"text\" name=\"ingresos\"></p>\n");
+      out.write("\n");
+      out.write("            <p><label>Gastos: </label><input type=\"text\" name=\"gastos\"></p>\n");
+      out.write("            <input type=\"submit\" value=\"Enviar\">\n");
+      out.write("        </form>\n");
+      out.write("        \n");
+      out.write("\n");
+      out.write("\n");
+      out.write("        ");
+ String nombre = request.getParameter("nombre");
+            String apellidos = request.getParameter("apellidos");
+            String estadoCivil = request.getParameter("estadoCivil");
+            String trabaja = request.getParameter("trabaja");
+            String ingresos = request.getParameter("ingresos");
+            String gastos = request.getParameter("gastos");
+           // Float ingresos= Float.parseFloat(request.getParameter("ingresos"));
+           // Float gastos = Float.valueOf(request.getParameter("gastos"));
+            
+      out.write("\n");
+      out.write("            ");
+
+            out.write("Ingresos: " + ingresos);
+
+//   Float gasto=Float.parseFloat(gastos);
+//   Float ingreso=Float.parseFloat(ingresos);
+//   
+//   out.write("Ingresos: " + ingreso); 
+            out.write("Gastos: " + gastos);
+//   Float ingreso = new Float(ingresos);
+//   Float gasto= new Float(gastos);
+            // String alfa= new String(“3.5”);
+
+//double ingreso= 0;
+//double gasto=0;
+//
+//ingreso= Double.parseDouble(ingresos);
+//gasto=Double.parseDouble(gastos);
+//String alfa8=new String(“50”);
+try{
+            Double ingreso = new Double(0);
+            Double gasto = new Double(0);
+
+            ingreso = ingreso.valueOf(ingresos);
+            gasto = gasto.valueOf(gastos);
+            if (ingreso > gasto) {
+
+                out.write("Ingreso es mayor que el gasto");
+            } else {
+
+                out.write("Gasto es mayor que el ingreso");
+            }
+
+//   ingresos = Float.parseFloat(request.getParameter("ingresos"));
+//   gastos=Float.parseFloat(request.getParameter("gastos"));
+}catch(Exception e){
+//   out.write(e.getMessage());
+}
+        
+      out.write("\n");
+      out.write("\n");
+      out.write("           ");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html>\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"estilo.css\">\r\n");
+      out.write("    </head>\r\n");
+      out.write("    <body>\r\n");
+      out.write("        <footer>\r\n");
+      out.write("            <p>Grupo&nbsp;08&nbsp;IT</p>\r\n");
+      out.write("            <p>Juan Antonio L&oacute;pez Cano, Susana de la Calle Iglesias</p>\r\n");
+      out.write("        </footer>\r\n");
+      out.write("    </body>\r\n");
+      out.write("</html>\r\n");
+      out.write("\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ epd01/p4/build/web/procesa.jsp
@@ -0,0 +1,104 @@
+<%-- 
+    Document   : index
+    Created on : 10-feb-2018, 12:21:56
+    Author     : 
+--%>
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+    <a href="procesa.jsp"></a>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>EPD1-p1</title>
+    </head>
+    <body>
+        <h1>Respuesta a su Solicitud de cr&eacute;dito</h1>
+        <!--                    Procesamiento del formulario-->
+
+        <!--Los campos se validan con expresiones regulares -->
+        <!--No se permiten campos vacíos. Se tratan espacios en blanco y caracteres permitidos-->
+        <%
+            final int DOBLE = 2;
+            String errores = "";
+            /*Esta expresión regular permite introducir letras mayúsculas y minúsculas y espacios en blanco. La longitud mínima para la cadena es 3 carácteres y la máxima es 30 carácteres*/
+            String patron1 = "^[a-zA-Z+[ x0Bf ]]{3,30}";
+            /*Esta expresión regular permite introducir números entre el 0 y 9 ambos incluídos, teniendo como longitud tanto la parte entera como la parte decimal de 0 a 3 dígitos*/
+            String patron2 = "[0-9]{0,6}.[0-9]{0,3}";
+//            [0-9]{0,6}.{0,1}[0-9]{0,3}
+            String nombre = request.getParameter("nombre");
+            String apellidos = request.getParameter("apellidos");
+            String estadoCivil = request.getParameter("estadoCivil");
+            String trabaja = request.getParameter("trabaja");
+            String ingresos = request.getParameter("ingresos");
+            String gastos = request.getParameter("gastos");
+
+            /*Validación  formulario*/
+            if (nombre.isEmpty()) {
+                errores = "<span class='contenido'>-El campo Nombres está vacío. Complételo por favor.</span><br />";
+            }
+            if (!nombre.matches(patron1)) {
+                errores += "<span class='contenido'>-El campo Nombres no se corresponde con el formato aceptado. Ejemplo de formato aceptado: Lorenzo Pascual</span><br />";
+            }
+            if (apellidos.isEmpty()) {
+                errores += "<span class='contenido'>-El campo Apellidos está vacío. Complételo por favor.</span><br />";
+            }
+            if (!apellidos.matches(patron1)) {
+                errores += "<span class='contenido'>-El campo Apellidos no se corresponde con el formato aceptado. Ejemplo de formato aceptado: Medina Soria</span><br />";
+            }
+            if (estadoCivil.equalsIgnoreCase("-")) {
+                errores += "<span class='contenido'>-El campo Estado Civil no se corresponde con ninguna de las opciones disponibles. Seleccione una opción por favor.</span><br />";
+            }
+            if (trabaja==null) {
+                errores += "<span class='contenido'>-El campo Trabaja no se corresponde con ninguna de las opciones disponibles. Seleccione una opción por favor.</span><br />";
+            }
+            if (ingresos.isEmpty()) {
+                errores += "<span class='contenido'>-El campo Ingresos está vacío. Complételo por favor.</span><br />";
+            }
+            if (!ingresos.matches(patron2)) {
+                errores += "<span class='contenido'>-El campo Ingresos no se corresponde con el formato aceptado. Ejemplo de fomato aceptado: 1543.987</span><br />";
+            }
+            if (gastos.isEmpty()) {
+                errores += "<span class='contenido'>-El campo Gastos está vacío. Complételo por favor.</span><br />";
+            }
+            if (!gastos.matches(patron2)) {
+                errores += "<span class='contenido'>-El campo Gastos no se corresponde con el formato aceptado. Ejemplo de formato aceptado: 860.56</span><br />";
+            }
+
+            try {
+                if (errores == "") {
+                    Float gasto = Float.parseFloat(gastos);
+                    Float ingreso = Float.parseFloat(ingresos);
+
+
+        %>
+        <span class="contenido">Nombre: <%=nombre%> </span><br />
+        <span class="contenido">Apellidos: <%=apellidos%> </span><br />
+        <span class="contenido">Estado Civil: <%=estadoCivil%> </span><br />
+        <span class="contenido">Trabaja: <%=trabaja%> </span><br />
+        <span class="contenido">Ingresos anuales: <%=ingreso%> €</span><br />
+        <span class="contenido">Gastos anuales: <%=gasto%> €</span><br /><br />
+        <%
+
+                    if (((!estadoCivil.equalsIgnoreCase("casado")) && trabaja.equals("si") && ingreso > gasto) || (estadoCivil.equals("casado") && trabaja.equals("si") && ingreso >= (DOBLE * gasto))) {
+                        out.write("<span class='concedido'>Crédito concedido</span>");
+                    } else {
+                        out.write("<span class='noConcedido'>No es posible concederle el crédito</span>");
+                    }
+                } else {
+                    out.write(errores);
+                }
+
+            } catch (Exception e) {
+                out.print(e.getMessage());
+            }
+        %>
+
+        <%@include file="footer.jsp" %>
+    </body>
+</html>

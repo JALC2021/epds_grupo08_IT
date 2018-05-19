b9d27b3eb526bf8d6ac46328a860c9c126709c92
jalc <jantonio2511@gmail.com>
2018-05-19 19:30:24 +0200

    reemplazo

---
+++ epd08/p7/build/generated-sources/ap-source-output/BBDD/Pelicula_.java
@@ -0,0 +1,19 @@
+package BBDD;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-05-14T19:00:11")
+@StaticMetamodel(Pelicula.class)
+public class Pelicula_ { 
+
+    public static volatile SingularAttribute<Pelicula, Date> fechaEstreno;
+    public static volatile SingularAttribute<Pelicula, String> idioma;
+    public static volatile SingularAttribute<Pelicula, Integer> id;
+    public static volatile SingularAttribute<Pelicula, Date> fechaVideo;
+    public static volatile SingularAttribute<Pelicula, String> nombre;
+    public static volatile SingularAttribute<Pelicula, String> pais;
+
+}
